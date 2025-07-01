548f7267a59a269d18cfd1f26b6ba10265f43742
Anthony Nicodemus <anthonynicodemus999@gmail.com>
2025-07-02 00:25:06 +0800

    Initial Commit

---
+++ js/nav_transition.js
@@ -0,0 +1,10 @@
+window.onscroll = function() {
+        if (window.scrollY <= 350) {
+     document.getElementById("topnav").style.backgroundColor = ""; 
+         
+    }
+    else    
+    {
+        document.getElementById("topnav").style.backgroundColor = "black"; 
+    }
+};
