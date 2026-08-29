4791e48a454e783f3c2ca7fe037daf03ba57b902
Anthony Nicodemus <anthonynicodemus999@gmail.com>
2026-08-29 23:53:18 +0800

    Added base path

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
     vueJsx(),
     vueDevTools(),
   ],
+  base: '/ncdmsanthony.github.io/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
